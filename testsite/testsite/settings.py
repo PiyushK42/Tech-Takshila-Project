468f8edcc1901f40561aeee321cef861b47a6ffc
Piyush <piyushbasuri966@gmail.com>
2019-06-23 23:40:47 +0530

    changes in settings.py

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -119,21 +119,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-'''
-AWS_ACCESS_KEY_ID = 'AKIAY47FH43FZC7X7BHP'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'site-static-files-storage'
-#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_CUSTOM_DOMAIN = 'dkx1y09of7apc.cloudfront.net'
-'''
-'''AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-'''
-'''
-AWS_LOCATION = 'static'
-AWS_DEFAULT_ACL = None
-'''
 
 
 # Static files (CSS, JavaScript, Images)
@@ -145,6 +130,4 @@ STATICFILES_DIRS = [
    STATIC_DIR,
 ]
 
-#STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-#STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
